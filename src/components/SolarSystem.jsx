9dd77a794166cefe4f889626f01c0d85bc933fbf
Lucas <bithome1@outlook.com>
2022-01-07 22:39:26 -0300

    Requisito 6 completo

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,4 +1,9 @@
+/** Source: https://app.betrybe.com/course/front-end/introducao-a-react/componentes-react/0115c033-cfc0-48bc-bcf5-812b599ee79a/conteudos/e80237ee-27b0-4e38-9941-c3ad4a18509c/lista-de-componentes-e-chaves/c557f0df-fdc4-4e9d-a787-e4545030eea6?use_case=side_bar */
+/** Source: https://app.betrybe.com/course/fundamentals/higher-order-functions-do-javascript-es6/javascript-es6-higher-order-functions-map-e-filter/eb53fd32-be11-4a70-a925-c29e1d7f8925/conteudos/3f220de0-5fea-47cf-8993-452b778d10b6/arraymap/4842d96e-a016-4dd4-bb3a-2feb7b1c2078?use_case=side_bar */
+/** Source: https://reactjs.org/docs/lists-and-keys.html#keys */
 import React from 'react';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
 
 class SolarSystem extends React.Component {
@@ -6,6 +11,11 @@ class SolarSystem extends React.Component {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((planet) => (
+          <div key={ planet.name }>
+            <PlanetCard planetName={ planet.name } planetImage={ planet.image } />
+          </div>
+        ))}
       </div>
     );
   }
