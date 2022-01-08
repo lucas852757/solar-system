d30d7ae41a6498a377bc3485071f6cb750b34817
Lucas <bithome1@outlook.com>
2022-01-08 18:54:33 -0300

    Requisito 9 completo

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,3 +1,5 @@
+/** Source: https://app.betrybe.com/course/front-end/introducao-a-react/componentes-react/0115c033-cfc0-48bc-bcf5-812b599ee79a/conteudos/e80237ee-27b0-4e38-9941-c3ad4a18509c/proptypes-checagem-de-tipos/de0f34ac-c606-4217-a738-d16a245da6a4?use_case=side_bar */
+/** Source: http://www.hackingwithreact.com/read/1/41/how-to-add-react-component-prop-validation-in-minutes */
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -14,7 +16,7 @@ class MissionCard extends React.Component {
     );
   }
 }
-MissionCard.propType = {
+MissionCard.propTypes = {
   name: PropTypes.string.isRequired,
   year: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
