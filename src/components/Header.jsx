e4fadd19fa808cc8eb3fc5bad0b522af2ca3ba56
Lucas <bithome1@outlook.com>
2023-11-08 23:43:23 -0300

    feat: Criado classe Title.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../Styles.css';
 
 class Header extends React.Component {
   render() {
-    return <header><h1>Sistema Solar</h1></header>;
+    return <header><h1 className="Title"> Sistema Solar</h1></header>;
   }
 }
 
