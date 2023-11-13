afd193694d2d13436311e17f4e6adc77a06fc0f1
Lucas <bithome1@outlook.com>
2023-11-13 19:15:19 -0300

    refactor: Ajustado código para se adaptar ao fexbox.

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import Title from './Title';
 import missions from '../data/missions';
 import MissionCard from './MissionCard';
+import '../Styles.css';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        {missions.map((mission) => (
-          <div key={ mission.name }>
-            <MissionCard
-              name={ mission.name }
-              year={ mission.year }
-              country={ mission.country }
-              destination={ mission.destination }
-            />
+        <div className="flex-container-position">
+          <div
+            className="flex-container-mission"
+          >
+            {missions.map((mission) => (
+              <MissionCard
+                key={ mission.name }
+                name={ mission.name }
+                year={ mission.year }
+                country={ mission.country }
+                destination={ mission.destination }
+              />
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     );
   }
