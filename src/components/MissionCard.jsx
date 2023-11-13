57957554c3aebc2887e1562a0d254e4d3e1edd53
Lucas <bithome1@outlook.com>
2023-11-13 19:09:10 -0300

    refactor: Substituído tag span por tag p.

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,17 +1,18 @@
 /** Source: https://app.betrybe.com/course/front-end/introducao-a-react/componentes-react/0115c033-cfc0-48bc-bcf5-812b599ee79a/conteudos/e80237ee-27b0-4e38-9941-c3ad4a18509c/proptypes-checagem-de-tipos/de0f34ac-c606-4217-a738-d16a245da6a4?use_case=side_bar */
 /** Source: http://www.hackingwithreact.com/read/1/41/how-to-add-react-component-prop-validation-in-minutes */
 import React from 'react';
+import '../Styles.css';
 import PropTypes from 'prop-types';
 
 class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <span data-testid="mission-name">{name}</span>
-        <span data-testid="mission-year">{year}</span>
-        <span data-testid="mission-country">{country}</span>
-        <span data-testid="mission-destination">{destination}</span>
+      <div className="mission" data-testid="mission-card">
+        <p data-testid="mission-name">{name}</p>
+        <p data-testid="mission-year">{year}</p>
+        <p data-testid="mission-country">{country}</p>
+        <p data-testid="mission-destination">{destination}</p>
       </div>
     );
   }
